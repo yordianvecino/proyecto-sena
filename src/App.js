aae8a2d3a1d60e2a8d8a3c28ce2bfa12bbd1f621
yordiianveciino <yordiian.veciino@hotmail.com>
2022-08-22 00:34:11 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Inicio from './Componentes/Inicio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h1>COLEGÍO JOSÉ MANUEL-RESTREPO - ANTIOQUIA - ARBOLETES</h1>
+        <hr />
+        <Switch>
+          <Route path='/' exact>
+            <Inicio/>
+          </Route>
+          <Route path='/boletin' exact>
+            BOLETIN INFORMATIVO Y EDUCATIVO
+          </Route>
+          <Route path='/certificados' exact>
+            CERTIFICADO ESCOLAR
+          </Route>
+          <Route path='/certificados/notas' exact>
+            CERTIFICADO DE NOTAS
+          </Route>
+          <Route path='/constancias' exact>
+            CONSTANCIAS
+          </Route>
+          <Route path='/preguntas' exact>
+            PREGUNTAS FRECUENTES
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
