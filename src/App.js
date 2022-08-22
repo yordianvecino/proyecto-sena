68e32fcb567b5f45948c6900a50e22b542661ff9
yordiianveciino <yordiian.veciino@hotmail.com>
2022-08-22 11:36:31 -0500

    Navar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Fragment} from 'react';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Inicio from './Componentes/Inicio';
 import Boletin from './Componentes/boletin';
 import Certificados from './Componentes/certificado';
@@ -13,6 +13,16 @@ function App() {
       <div className="container">
         <h1>COLEGÍO JOSÉ MANUEL-RESTREPO - ANTIOQUIA - ARBOLETES</h1>
         <hr />
+        <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+          <div class="btn-group" role="group" aria-label="First group">
+            <Link to="/" class="btn btn-dark">INICIO</Link>
+            <Link to="/boletin" class="btn btn-dark">BOLETIN INFORMATIVO Y EDUCATIVO</Link>
+            <Link to="/certificados" class="btn btn-dark">CERTIFICADO ESCOLAR</Link>
+            <Link to="/certificados/notas" class="btn btn-dark">CERTIFICADO DE NOTAS</Link>
+            <Link to="/constancias" class="btn btn-dark"> CONSTANCIAS</Link>
+            <Link to="/preguntas" class="btn btn-dark">PREGUNTAS FRECUENTES</Link>
+          </div>
+        </div>
         <Routes>
           <Route path='/' element={<Inicio />} exact></Route>
           <Route path='/boletin' element={<Boletin />}></Route>
